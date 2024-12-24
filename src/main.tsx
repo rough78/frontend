eb2741f11912ae75a751c8b56ec437c8818e2968
rough78 <3dfxcom@naver.com>
2024-12-24 18:20:32 +0900

    styles 이동

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
-import "./assets/styles/main.scss";
+import "./app/styles/main.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

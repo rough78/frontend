b6bf5bde6b2cddeeba4dd18dc35175e7b839d8f0
rough78 <3dfxcom@naver.com>
2024-12-31 15:48:30 +0900

    fix: 소소한 수정들

--- src/app/routers/appRouter.tsx
+++ src/app/routers/appRouter.tsx
@@ -31,7 +31,7 @@ export const AppRouter = () => {
                 index 
                 element={
                     <ProtectedRoute>
-                        <Home />
+                        <RevivewList />
                     </ProtectedRoute>
                 } 
             />

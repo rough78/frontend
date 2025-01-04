6bd04c0274f22bd81dc427e5cd1e01f2740e212b
lsh <jetaime12121@gmail.com>
2025-01-04 20:45:05 +0900

    feature: review-filter layout

---
+++ src/entities/review/ui/reviewFilter/ReviewFilter.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import styles from "./ReviewFilter.module.scss";
+import Button from "@/shared/ui/button/ui/Button";
+import filter from "@shared/assets/images/filter.svg";
+
+const ReviewFilter = () => {
+  const [activeFilter, setActiveFilter] = useState<"latest" | "highRating">(
+    "latest"
+  );
+  const handleFilterClick = (filterType: "latest" | "highRating") => {
+    setActiveFilter(filterType);
+  };
+  return (
+    <div className={styles.reviewFilter}>
+      <div className={styles.reviewFilter__btnWraper}>
+        <Button
+          className={`${styles.reviewFilter__filterBtn} ${activeFilter === "latest" ? styles.active : ""}`}
+          text="최신순"
+          altText="최신순으로 확인하기"
+          onClick={() => handleFilterClick("latest")}
+        />
+        <Button
+          className={`${styles.reviewFilter__filterBtn} ${activeFilter === "highRating" ? styles.active : ""}`}
+          text="평점 높은 순"
+          altText="평점 높은 순으로 확인하기"
+          onClick={() => handleFilterClick("highRating")}
+        />
+      </div>
+      <Button imgUrl={filter} altText="태그 필터링 버튼" className="imgBtn" />
+    </div>
+  );
+};
+
+export default ReviewFilter;
--- src/pages/ReviewList.tsx
+++ src/pages/ReviewList.tsx
@@ -1,8 +1,10 @@
 import { ReviewItem } from "@/entities/review/ui";
+import ReviewFilter from "@/entities/review/ui/reviewFilter/ReviewFilter";
 
 const ReviewList = () => {
   return (
     <div>
+      <ReviewFilter />
       <ReviewItem />
     </div>
   );

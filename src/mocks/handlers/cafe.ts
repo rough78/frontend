e8722097b8d89a74d63ebcfe31a0e2adfdb863de
rough78 <3dfxcom@naver.com>
2025-01-14 12:47:02 +0900

    msw 수정

--- src/mocks/handlers/cafe.ts
+++ src/mocks/handlers/cafe.ts
@@ -1,6 +1,5 @@
 import { http, HttpResponse } from "msw";
 import { dummyCafes } from "@shared/api/cafe/mockData";
-import { CafeMapper } from "@shared/api/cafe/mapper/cafeMapper";
 
 export const cafeHandlers = [
   http.get("/api/cafes/search", ({ request }) => {
@@ -8,8 +7,7 @@ export const cafeHandlers = [
     const searchName = url.searchParams.get("name")?.toLowerCase();
 
     const filteredCafes = dummyCafes
-      .filter(cafe => (cafe.title ?? "").toLowerCase().includes(searchName || ""))
-      .map(cafe => CafeMapper.toICafeDescription(cafe));
+      .filter(cafe => (cafe.title ?? "").toLowerCase().includes(searchName || ""));
 
     return HttpResponse.json({ items: filteredCafes });
   })

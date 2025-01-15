e3ce64b45272a4aee92e6fb9890c31c9a901525e
rough78 <3dfxcom@naver.com>
2025-01-15 12:31:24 +0900

    cafeinfo style

--- src/entities/cafeInfo/ui/CafeInfo.tsx
+++ src/entities/cafeInfo/ui/CafeInfo.tsx
@@ -1,10 +1,19 @@
 import PhotoSwiper from "@shared/ui/photoSwiper/PhotoSwiper.tsx";
+import CafeInfoItem from "./CafeInfoItem";
 import styles from './CafeInfo.module.scss';
 
 const CafeInfo = () => {
   return (
-    <div className={styles.photoSwiperOverride}>
-      <PhotoSwiper showChips={false} />
+    <div>
+      <div className={styles.photoSwiperOverride}>
+        <PhotoSwiper showChips={false} />
+      </div>
+      <CafeInfoItem
+        name="로우키"
+        address="서울 성동구 연무장 3길 6 (성수동 2가)"
+        instaLink="https://instagram.com/cafename"
+        onBookmarkClick={() => console.log('Bookmark clicked')}
+      />
     </div>
   );
 };

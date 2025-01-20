f6e8fd4a660657c3260e19b2a142a1c4fe1b350f
rough78 <3dfxcom@naver.com>
2025-01-20 18:13:07 +0900

    fix: Footer 떨림효과, 프로필 flickering 수정

--- src/entities/profile/ui/profileHeader/ProfileHeader.tsx
+++ src/entities/profile/ui/profileHeader/ProfileHeader.tsx
@@ -6,20 +6,21 @@ const ProfileHeader = ({ isScrolled }: ProfileHeaderProps) => {
   return (
     <div
       className={`${styles.profileHead} ${
-        isScrolled ? styles["profileHead--small"] : styles["profileHead--large"]
+        isScrolled ? styles["profileHead--shrink"] : ""
       }`}
     >
-      <img
-        className={styles.profileHead__profileImg}
-        src={MyProfileImage}
-        alt="myProfileImage"
-      />
-      <div className={styles.profileHead__meta}>
-        <p className={styles.profileHead__nickName}>카페탐방</p>
-        <p className={styles.profileHead__introduction}>
-          맛있는 커피를 찾아
-          다닙니다sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-        </p>
+      <div className={styles.profileHead__inner}>
+        <img
+          className={styles.profileHead__profileImg}
+          src={MyProfileImage}
+          alt="myProfileImage"
+        />
+        <div className={styles.profileHead__meta}>
+          <p className={styles.profileHead__nickName}>카페탐방</p>
+          <p className={styles.profileHead__introduction}>
+            맛있는 커피를 찾아 다닙니다
+          </p>
+        </div>
       </div>
     </div>
   );
